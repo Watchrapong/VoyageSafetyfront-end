6c8e1a05b2fe4fee7bdbc390f122b2acea3316bf
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-05-22 16:43:07 +0700

    เพิ่มปุ่มดูแผนที่

--- src/components/establishdetail/establishdetail.js
+++ src/components/establishdetail/establishdetail.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./establishdetail.css";
 
 class Establishdetail extends Component {
   render() {
-    return <div>Establishdetail</div>;
+    return (
+    <div>Establishdetail  
+      <div className="bg">
+      <div className="est-profile-box">
+        <p>kuay</p>
+      </div>
+      <div className="est-subprofile-box">
+      </div>
+      <div className="info-box">
+      </div>
+      <div className="vaccination-box">
+      </div>
+      <div className="map-box">
+        <div>
+          <button className="map-button map-text">ดูแผนที่</button>
+        </div>
+      </div>
+      </div>
+    </div>
+    );
   }
 }
 
