dc0c47556e66ba15fd056d975eca0026fefba4e6
Oat <rachawas.092542@mail.kmutt.ac.th>
2021-08-03 00:00:42 +0700

    login หน้าแต่ง

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { login, autoLogin } from "./../../actions/login.action";
 import { connect } from "react-redux";
-import { Container, Row, Col, Form, Button, Badge } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
-import bglogin from "./../../assets/img/bglogin.png";
+import {Container, Row, Col, Form, Button} from "react-bootstrap"
+import Image from "react-bootstrap/Image"
+import bglogin from "./../../assets/img/bglogin.png"
+import "../login/login.css"
 
 class Login extends Component {
+
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       Email: "",
       Password: "",
-    };
+    }
   }
 
   componentDidMount() {
@@ -21,6 +23,7 @@ class Login extends Component {
 
   showError = () => {
     return (
+      
       <div className="alert alert-danger alert-dismissible">
         <button
           type="button"
@@ -29,147 +32,76 @@ class Login extends Component {
           aria-hidden="true"
         >
           ×
-        </button>
+                  </button>
         <h5>
           <i className="icon fas fa-ban" /> Error!
-        </h5>
-        Incorrect Email or Password
+                  </h5>
+                  Incorrect Email or Password
       </div>
-    );
-  };
+    )
+  }
+
+
 
   render() {
     return (
       <div>
-        <Container>
-          <Row>
-            <Col>
-              <Image src={bglogin} fluid />
-            </Col>
-            <Col>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" onChange={e => this.setState({ Email: e.target.value })}/>
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" onChange={e => this.setState({ Password: e.target.value })}/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                {this.props.loginReducer.isError && this.showError()}
-                <Button variant="primary" type="submit" onClick={e => {
-                        e.preventDefault();
-                        this.props.login(this.props.history, this.state)
-                      }}>
-                  ลงชื่อเข้าใช้
-                </Button>
-              </Form>
-              <a href="" onClick={() => this.props.history.push("/register")}>
-              <Badge pill bg="light" text="dark"
-                  >
-                    ยังไม่ได้สมัครสมาชิกสำหรับเข้าใช้งานเว็บไซต์ ?
-                </Badge></a>
-            </Col>
-          </Row>
-        </Container>
-        {/* <div className="hold-transition login-page">
-        <div className="login-box">
-         
-          <div className="card card-outline card-primary">
-            <div className="card-header text-center">
-              <a href="../../index2.html" className="h1">
-                <b>Voyage</b>Safety
-              </a>
-            </div>
-            <div className="card-body">
-              <p className="login-box-msg">Sign in to start your session</p>
-              <form>
-                <div className="input-group mb-3">
-                  <input
-                    onChange={e => this.setState({ Email: e.target.value })}
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                    name="Email"
-                  />
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <span className="fas fa-envelope" />
-                    </div>
-                  </div>
-                </div>
-                <div className="input-group mb-3">
-                  <input
-                    onChange={e => this.setState({ Password: e.target.value })}
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                    name="Password"
-                  />
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <span className="fas fa-lock" />
-                    </div>
-                  </div>
-                </div>
-                {this.props.loginReducer.isError && this.showError()}
-                <div className="row">
-                  <div className="col-8">
-                    <div className="icheck-primary">
-                      <input type="checkbox" id="remember" />
-                      <label htmlFor="remember">Remember Me</label>
-                    </div>
-                  </div>
-                  
-                  <div className="col-4">
-                    <button
-                      onClick={e => {
-                        e.preventDefault();
-                        this.props.login(this.props.history, this.state)
-                      }}
-                      type="submit" className="btn btn-block btn-primary btn-sm">
-                      Sign In
-                    </button>
-                  </div>
-                  
-                </div>
-              </form>
-              <p className="mb-1">
-                <button className="btn btn-block btn-outline-primary btn-sm">
-                  I forgot my password
-                </button>
-              </p>
-              <p className="mb-0"> */}
-        {/* <button
-                  onClick={() => this.props.history.push("/register")}
-                  className="btn btn-block btn-outline-primary btn-sm"
-                >
-                  Register a new membership
-                </button>
-              </p>
-            </div>
-            
-          </div>
-         
+        <section className="u-align-center u-clearfix sectionlogin" id="sec-2614">
+  <div className="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+    <div className="u-layout">
+      <div className="u-layout-row">
+        <div className="u-container-style u-image u-layout-cell u-size-30 u-image-1" data-image-width={896} data-image-height={1080}>
+          <div className="u-container-layout u-container-layout-1" />
         </div>
-      </div> */}
+        <div className="u-container-style u-layout-cell u-shape-rectangle u-size-30 u-layout-cell-2">
+          <div className="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-container-layout-2">
+            <div className="u-container-style u-group u-group-1">
+              <div className="u-container-layout">
+                <h1 className="u-text u-text-default u-text-1">กรุณาลงชื่อเข้าใช้ </h1>
+                <div className="u-border-3 u-border-grey-dark-1 u-expanded-width u-line u-line-horizontal u-line-1" />
+                <div className="u-form u-form-1">
+                  <form action="#" method="POST" className="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" source="custom" name="form" style={{padding: 10}}>
+                    <div className="u-form-group u-form-group-1">
+                      <label htmlFor="text-42dd" className="u-label">อีเมลล์</label>
+                      <input type="text" placeholder="Enter you Email" id="text-42dd" name="text" className="u-border-1 u-border-grey-30 u-input u-input-rectangle u-radius-10 u-white" />
+                    </div>
+                    <div className="u-form-group u-form-group-2">
+                      <label htmlFor="text-364e" className="u-label">รหัสผ่าน</label>
+                      <input type="text" placeholder="Enter your password" id="text-364e" name="password" className="u-border-1 u-border-grey-30 u-input u-input-rectangle u-radius-10 u-white" required="required" />
+                    </div>
+                    <div className="u-align-center u-form-group u-form-submit u-form-group-3">
+                      <a href="#" className="u-btn u-btn-submit u-button-style">Submit</a>
+                      <input type="submit" defaultValue="submit" className="u-form-control-hidden" />
+                    </div>
+                    <div className="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+                    <div className="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+                    <input type="hidden" defaultValue name="recaptchaResponse" />
+                  </form>
+                </div>
+                <p className="u-text u-text-default u-text-2">ลืมรหัสผ่าน</p>
+                <p className="u-text u-text-default u-text-3">ยังไม่ได้สมัครสมาชิคสำหรับเข้าใช้งานเว็บไซต์?</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  </div>
+</section>
+
+      </div>
+     
+
+      );
   }
 }
 
-const mapStateToProps = ({ loginReducer }) => ({ loginReducer });
+const mapStateToProps = ({ loginReducer }) => ({ loginReducer })
 
 const mapDispatchToProps = {
   login,
-  autoLogin,
-};
+  autoLogin
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
