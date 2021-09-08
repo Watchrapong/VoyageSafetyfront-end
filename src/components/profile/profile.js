b52db4d7085a024fc98a1997c7cd44438c3e516a
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-09-08 18:10:43 +0700

    fix2

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -4,12 +4,8 @@ import blankprofile from "../../assets/img/blankprofile.png";
 import { server, YES } from "../../constants";
 import { httpClient } from "../../utils/HttpClient";
 import { Button } from "react-bootstrap";
-<<<<<<< Updated upstream
-import Modal from "react-bootstrap/modal";
-=======
 import Modal from "react-bootstrap/Modal";
 import Alert from "react-bootstrap/Alert";
->>>>>>> Stashed changes
 import FormData from "form-data";
 
 class Profile extends Component {
@@ -191,8 +187,6 @@ class Profile extends Component {
                       width={220}
                       height={220}
                     />
-<<<<<<< Updated upstream
-=======
 
                     <div className="input-group mb-3">
                       <input type="file" className="form-control" />
@@ -204,7 +198,6 @@ class Profile extends Component {
                       </label>
                     </div>
 
->>>>>>> Stashed changes
                     <div className="row g-5">
                       <div className="col-md-12 col-lg-12">
                         <h4 className="mb-3">ประวัติส่วนตัว</h4>
